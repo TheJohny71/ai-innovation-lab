51b5b0d39ae20704478a04cee693add7f09820a7
TheJohny71 <jduval71@me.com>
2024-12-21 22:58:57 +0000

    Update next config for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true
-  }
+  },
+  basePath: '/ai-innovation-lab',
+  assetPrefix: '/ai-innovation-lab'
 }
 
 module.exports = nextConfig
