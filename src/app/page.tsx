ea122bb5a869c55912ab89c0178180c03d1c2308
TheJohny71 <jduval71@me.com>
2024-12-21 21:55:15 +0000

    Update configuration: Add Tailwind and PostCSS configs, update package files and page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import PresentationDeck from '@/components/layout/PresentationDeck';
+import PresentationDeck from './components/layout/PresentationDeck';
 
 export default function Home() {
   return <PresentationDeck />;
