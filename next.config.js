b07d266913ed118b8ee5b118d632c6679a68dcbe
TheJohny71 <jduval71@me.com>
2024-12-21 22:08:49 +0000

    updates

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/ai-innovation-lab' : '',
   images: {
     unoptimized: true
   }
