1edc772f4de53a2174fa2213137df5fa489752b0
TheJohny71 <jduval71@me.com>
2024-12-30 17:24:49 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/ai-innovation-lab',
   images: {
     unoptimized: true,
   },
-  assetPrefix: '/ai-innovation-lab/',
+  basePath: '/ai-innovation-lab',
 }
 
 module.exports = nextConfig
